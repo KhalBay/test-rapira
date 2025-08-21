ee7b4ca4cc8309a0a46a1ecf83bb1b858ee21d39
Egor <jagyar37@gmail.com>
2025-08-21 17:58:44 +0800

    new build

--- vite.config.ts
+++ vite.config.ts
@@ -6,20 +6,19 @@ import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
-export default defineConfig({
-  base: '/test-rapira/',
-  build: {
-    outDir: 'docs',
-    emptyOutDir: true
-  },
-  plugins: [
-    vue(),
-    vueDevTools(),
-    tailwindcss(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default defineConfig(({ command }) => {
+  const isBuild = command === 'build'
+  return {
+    base: isBuild ? '/test-rapira/' : '/',
+    build: {
+      outDir: 'docs',
+      emptyOutDir: true
     },
-  },
+    plugins: [vue(), vueDevTools(), tailwindcss()],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      },
+    },
+  }
 })
